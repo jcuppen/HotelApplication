b46f63720c47b601d1815cfe46ea6a79a86c131c
JasonLighthunter <jobcuppen@live.nl>
2014-10-15 17:45:13 +0200

    Added reservationstarter class

---
+++ HotelApplication/Models/ReservationStarter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelApplication.Models
+{
+	public class ReservationStarter
+	{
+		public int NumberOfPeople { get; set; }
+		public DateTime Begin { get; set; }
+		public DateTime  End { get; set; }
+	}
+}
